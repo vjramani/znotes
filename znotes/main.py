e1abe8b329ea7ab0381f7ebbe608acaede915569
Vijay Venkatramani <vjramani@gmail.com>
2024-06-03 22:41:21 +0530

    added function to load and build tag index

--- znotes/main.py
+++ znotes/main.py
@@ -13,3 +13,98 @@
 
 if __name__ == "__main__":
     pass
+
+
+
+
+
+
+
+
+
+
+
+
+# import markdown_to_json
+#
+# value = """
+# # Nested List
+#
+# - Item 1
+#     - Item 1.1
+# - Item 2
+#     - Item 2.1
+#     - Item 2.2
+#         - Item 2.2.1
+# """
+
+# The simple way:
+# dictified = markdown_to_json.dictify(value)
+# assert dictified == {'Nested List': ['Item 1', ['Item 1.1'], 'Item 2']}
+# print(dictified)
+
+# Or, if you want a json string
+# jsonified = markdown_to_json.jsonify(value)
+# assert jsonified == """{"Nested List": ["Item 1", ["Item 1.1"], "Item 2"]}"""
+# print(jsonified)
+
+
+# read the tags from the tag section of a given note
+def read_note_tags(fhandle):
+    # print("read_note_tags")
+    skip_till_line_start(fhandle, "tags:")
+    val = read_till_line_start(fhandle, "---")
+    val = val.replace("\n","").replace("\r", "").replace("tags:","").replace(" ","")
+    return list(filter(None, val.split("#")))
+
+# read all tags from the global list of tags
+def read_tags_and_references(fhandle):
+    # print("read_tags_and_references")
+    tr = {}
+    skip_till_line_start(fhandle, "- ")
+    while line := fhandle.readline():
+        t = line.replace("- ", "").strip()
+        links = read_till_line_start(fhandle, "- ").replace("\t-", "").split("\n")
+        links = set(map(lambda x: x.strip(), filter(None, links)))
+        tr[t] = links
+
+    return tr
+
+
+def skip_till_line_start(fhandle, sentinel):
+    last_pos = fhandle.tell()
+    while line := fhandle.readline():
+        if line.find(sentinel) == 0:
+            break;
+        last_pos = fhandle.tell()
+    fhandle.seek(last_pos)
+
+def read_till_line_start(fhandle, sentinel):
+    last_pos = fhandle.tell()
+    fcontent = ""
+    while line := fhandle.readline():
+        if line.find(sentinel) == 0:
+            break;
+        fcontent += line
+        last_pos = fhandle.tell()
+    fhandle.seek(last_pos)
+
+    return fcontent
+
+def add_note_ref(tag_index, note_ref, tags):
+    for t in tags:
+        if t not in tag_index.keys():
+            tag_index[t] = set()
+
+        tag_index[t].add(note_ref);
+    return tag_index
+
+def update_tag_index(tag_index, notes_list):
+    for n in notes_list:
+        fobj = open(n, 'r')
+        ntags = read_note_tags(fobj)
+        fobj.close()
+        add_note_ref(tag_index, n, ntags) 
+
+    return tag_index
+
--- znotes/run.py
+++ znotes/run.py
@@ -1,4 +1,5 @@
-from read_data import *
+from main import *
+import os.path
 
 # test_note = "../sample_notes/note_1715024200.md"
 #
@@ -8,9 +9,34 @@ from read_data import *
 #
 # print(ddata)
 
-tag_list = "../test-files/tags.md"
-file_obj = open(tag_list, "r")
-ddata = read_tags_and_references(file_obj)
-file_obj.close()
+# tag_list = "../test-files/tags.md"
+# file_obj = open(tag_list, "r")
+# ddata = read_tags_and_references(file_obj)
+# file_obj.close()
+#
+# print(ddata)
 
-print(ddata)
+
+notes_list = ["../sample_notes/note_1715024200.md","../sample_notes/note_1715064425.md","../sample_notes/local_pdf_editing_suite_1715924445.md"]
+tag_index_file = "../tag_index.md"
+
+# Load the tag index data, check if the file is present and if not initialize empty tag index
+# Load each note and extract tags
+# For each tag append a reference to the note to the list
+# Write the tag index data to the tag_index file
+
+tag_index = {}
+if os.path.exists(tag_index_file):
+    file_obj = open(tag_index_file, 'r')
+    tag_index = read_tags_and_references(file_obj)
+    file_obj.close()
+
+print(f"tag_index: {tag_index}")
+
+tag_index = update_tag_index(tag_index, notes_list)
+
+print("tag_index:")
+for t in tag_index:
+    print(f"Tag: {t}")
+    for n in tag_index[t]:
+        print(f"\t {n}")
