72d128473b4d133eaf020e2a6e99f35dd2bf3774
Vijay Venkatramani <vjramani@gmail.com>
2024-06-06 13:25:35 +0530

    implemented new note creation from template and invoking editor

--- znotes/main.py
+++ znotes/main.py
@@ -1,21 +1,78 @@
 __VAULT_ROOT__ = "../vault/"
 __VAULT_NOTES__ = "atomic-notes/"
+__NEW_NOTE__ = "atomic-notes/"
 __NOTE_TEMPLATE__ = "utils/note-template.md"
 __TAG_INDEX__ = "utils/tag_index.md"
 
 if __name__ == "__main__":
     pass
 
+def build_and_update_tag_index():
+    notes_list = get_note_paths()
+    tag_index_file = get_tag_index_path()
+    tag_index = {}
+    fobj = read_file(tag_index_file)
+    if fobj is not None:
+        tag_index = read_tags_and_references(fobj)
+        fobj.close()
+
+    tag_index = update_tag_index(tag_index, notes_list)
+    write_tag_index(tag_index, tag_index_file)
+
+def open_note_for_editing(path):
+    import subprocess
+    subprocess.call(['nvim', '+7', path])
+
+
+def create_new_note_from_template(title):
+    nn_content = create_new_note(title)
+    nn_path = write_new_note(title, nn_content)
+    open_note_for_editing(nn_path)
+
 
 def get_tag_index_path():
-    return "{vault_root}{tag_index_path}".format(vault_root = __VAULT_ROOT__, tag_index_path = __TAG_INDEX__)
+    return f"{__VAULT_ROOT__}{__TAG_INDEX__}"
 
 def get_note_paths():
     import os
-    notes_folder = "{notes_path}".format(notes_path = __VAULT_NOTES__)
+    notes_folder = f"{__VAULT_NOTES__}"
     lf = [notes_folder+f.name for f in os.scandir(__VAULT_ROOT__+notes_folder) if f.is_file()]
-
     return lf
+
+def get_note_template_path():
+    return f"{__VAULT_ROOT__}{__NOTE_TEMPLATE__}"
+
+def get_new_note_path():
+    return f"{__VAULT_ROOT__}{__NEW_NOTE__}"
+
+def create_new_note(title):
+    import datetime
+    dt = datetime.datetime.today()
+    d = dt.strftime('%Y-%m-%d')
+    t = dt.strftime('%H:%M')
+    fobj = read_file_or_fail(get_note_template_path())
+    ncontent = fobj.read()
+    fobj.close()
+    ncontent = ncontent.replace("{{title}}", title)
+    ncontent = ncontent.replace("{{date}}", d).replace("{{time}}", t)
+    return ncontent
+    
+def gen_new_note_filename(title):
+    import time
+    ts = round(time.time()*1000)
+    fn = title.strip().replace(" ", "_")
+
+    return f"{fn}_{ts}.md"
+
+def write_new_note(title, nn_content):
+    nn_path = get_new_note_path()
+    nn_filename = gen_new_note_filename(title)
+    nn_path = f"{nn_path}{nn_filename}"
+    fobj = open(nn_path, 'w')
+    fobj.write(nn_content)
+    fobj.close()
+    return nn_path
+
 
 # read the tags from the tag section of a given note
 def read_note_tags(fhandle):
--- znotes/run.py
+++ znotes/run.py
@@ -1,64 +1,6 @@
 from main import *
 
-# test_note = "../sample_notes/note_1715024200.md"
-#
-# file_obj = open(test_note, "r")
-# ddata = read_note_tags(file_obj)
-# file_obj.close()
-#
-# print(ddata)
-
-# tag_list = "../test-files/tags.md"
-# file_obj = open(tag_list, "r")
-# ddata = read_tags_and_references(file_obj)
-# file_obj.close()
-#
-# print(ddata)
-
-
-# notes_list = [
-#         "../sample_notes/note_1715024200.md",
-#         "../sample_notes/note_1715064425.md",
-#         "../sample_notes/local_pdf_editing_suite_1715924445.md",
-#         "../sample_notes/load_vim_buffer_from_stdin_1715270271.md",
-#         "../sample_notes/visual_block_editing_in_vim_1716133283.md",
-#         "../sample_notes/re-arrange_files_and_folders_in_nvim-tree_1716542092.md",
-#         "../sample_notes/neovim_get_file_path_of_current_buffer_1716875414.md",
-#         "../sample_notes/run_zsh_command_from_neo-vim_key_bindings_1716874907.md"
-#         ]
-# tag_index_file = "../tag_index.md"
-
-# Load the tag index data, check if the file is present and if not initialize empty tag index
-# Load each note and extract tags
-# For each tag append a reference to the note to the list
-# Write the tag index data to the tag_index file
-
-# notes_list = get_note_paths()
-# tag_index_file = get_tag_index_path()
-# tag_index = {}
-# fobj = read_file(tag_index_file)
-# if fobj is not None:
-#     tag_index = read_tags_and_references(fobj)
-#     fobj.close()
-#
-# tag_index = update_tag_index(tag_index, notes_list)
-#
-# write_tag_index(tag_index, tag_index_file)
-#
-# for t in tag_index:
-#     print(f"Tag: {t}")
-#     for r in tag_index[t]:
-#         print(f"    {r}")
-
-
-# print(get_note_paths())
-
-import subprocess
-import time
-
-subprocess.call(['nvim', '+7', '../vault/utils/note-template.md'])
-time.sleep(10)
-print("done")
+create_new_note_from_template("some note title")
 
 
 
