e30bece85099e95add7f94779a6c0630026b00e5
Vijay Venkatramani <vjramani@gmail.com>
2024-08-21 15:20:22 +0530

    initial tests for expression evaluation passed

--- znotes/run.py
+++ znotes/run.py
@@ -43,44 +43,46 @@ def process_expr(expr, val_table):
                 last_op_order = op_order
             else:
                 while op_order <= last_op_order:
+                    # print(f"last_op_order: {last_op_order} op_order: {op_order}")
                     sr = apply_op(vals.pop(), vals.pop(), ops.pop())
                     vals.append(sr)
                     last_op_order = get_op_order(ops[-1]) if len(ops) > 0 else 0
                 ops.append(t)
+                last_op_order = get_op_order(ops[-1])
         elif t in val_table:
             vals.append(val_table[t])
         else:
             raise Exception(f"Unkown token '{t}'")
-        print(ops)
-        print(vals)
+        # print(ops)
+        # print(vals)
+        # print(f"last_op_order: {last_op_order} op_order: {op_order}")
 
     while len(ops) > 0:
         sr = apply_op(vals.pop(), vals.pop(), ops.pop())
         vals.append(sr)
+        # print(vals)
 
     return vals.pop()
 
 
 values = {"sa":{"a", "b", "c"}, "sb":{"a", "i"}, "sc":{"e", "f", "g", "c"}}
 
-# print(((values["sa"] & values["sb"]) | values["sc"]) - values["sa"])
-
 strexp = "sa * sb + sc - sa"
 result = process_expr(strexp, values)
 print(f"{result}")
 assert result == {'e', 'f', 'g'}
 
-# strexp = "sa * sb - sc + sa"
-# result = process_expr(strexp, values)
-# assert result == {'a', 'b', 'c'}
-# print(f"{result}")
-#
-# strexp = "sa + sb * sc - sa"
-# result = process_expr(strexp, values)
-# assert result == {'a', 'b', 'i'}
-# print(f"{result}")
-#
-# strexp = "sa + sb - sc * sa"
-# result = process_expr(strexp, values)
-# assert result == {'a', 'b', 'i'}
-# print(f"{result}")
+strexp = "sa * sb - sc + sa"
+result = process_expr(strexp, values)
+print(f"{result}")
+assert result == set()
+
+strexp = "sa + sb * sc - sa"
+result = process_expr(strexp, values)
+print(f"{result}")
+assert result == set()
+
+strexp = "sa + sb - sc * sa"
+result = process_expr(strexp, values)
+print(f"{result}")
+assert result == {'a', 'b', 'i'}
