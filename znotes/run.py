4c9198e9cacc435336d275523e44e7190f457e46
Vijay Venkatramani <vjramani@gmail.com>
2024-12-24 14:21:44 -0800

    WIP: partial commit

--- znotes/run.py
+++ znotes/run.py
@@ -11,10 +11,10 @@ from main import *
 # a - bcde
 
 
-ops = ['-', '+', '*']
+operators = [')','-', '+', '*', '(']
 
 def get_op_order(op):
-    return ops.index(op)+1 if op in ops else -1
+    return operators.index(op) if op in operators else -1
 
 def apply_op(sb, sa, op):
     match op:
@@ -27,62 +27,92 @@ def tokenize(expstr):
     ret = expstr.split(' ')
     return ret
 
+def next_token(tokens):
+    if len(tokens) <= 0:
+        return None, 0
+    t = tokens.pop(0)
+    o = get_op_order(t)
+    return t, o
 
 def process_expr(expr, val_table):
+    highest_op_order = len(operators)-1
     ops = []
     vals = []
-    last_op_order = 0
-
     tokens = tokenize(expr)
-    for t in tokens:
-        op_order = get_op_order(t)
-        # print(f"processing token: {t} with order: {op_order}")
-        if op_order > 0:
-            if op_order > last_op_order:
-                ops.append(t)
-                last_op_order = op_order
-            else:
-                while op_order <= last_op_order:
-                    # print(f"last_op_order: {last_op_order} op_order: {op_order}")
-                    sr = apply_op(vals.pop(), vals.pop(), ops.pop())
-                    vals.append(sr)
-                    last_op_order = get_op_order(ops[-1]) if len(ops) > 0 else 0
-                ops.append(t)
-                last_op_order = get_op_order(ops[-1])
-        elif t in val_table:
-            vals.append(val_table[t])
-        else:
-            raise Exception(f"Unkown token '{t}'")
-        # print(ops)
-        # print(vals)
-        # print(f"last_op_order: {last_op_order} op_order: {op_order}")
+    last_op_order = 0
+    curr_token = tokens.pop(0)
+    curr_op_order = get_op_order(curr_token)
+    running = True
 
-    while len(ops) > 0:
-        sr = apply_op(vals.pop(), vals.pop(), ops.pop())
-        vals.append(sr)
-        # print(vals)
+    while running:
+        if len(tokens) + len(ops) <= 0:
+            running = False
+            continue
+        if curr_op_order < 0:
+            vals.append(val_table[curr_token])
+            curr_token, curr_op_order = next_token(tokens)
+            continue
+        if curr_op_order >= last_op_order:
+            ops.append(curr_token)
+            last_op_order = 0 if curr_op_order == highest_op_order else curr_op_order
+            curr_token, curr_op_order = next_token(tokens)
+            continue
+        if curr_op_order < last_op_order:
+            if last_op_order < highest_op_order:
+                sr = apply_op(vals.pop(), vals.pop(), ops.pop())
+                vals.append(sr)
+            else:
+                ops.pop()
+                curr_token, curr_op_order = next_token(tokens)
+            last_op_order = get_op_order(ops[-1]) if len(ops) > 0 else 0
+            continue
 
     return vals.pop()
 
 
 values = {"sa":{"a", "b", "c"}, "sb":{"a", "i"}, "sc":{"e", "f", "g", "c"}}
 
+
 strexp = "sa * sb + sc - sa"
 result = process_expr(strexp, values)
-print(f"{result}")
+print(f"Result: {result}")
 assert result == {'e', 'f', 'g'}
 
 strexp = "sa * sb - sc + sa"
 result = process_expr(strexp, values)
-print(f"{result}")
+print(f"Result: {result}")
 assert result == set()
 
 strexp = "sa + sb * sc - sa"
 result = process_expr(strexp, values)
-print(f"{result}")
+print(f"Result: {result}")
 assert result == set()
 
 strexp = "sa + sb - sc * sa"
 result = process_expr(strexp, values)
-print(f"{result}")
+print(f"Result: {result}")
 assert result == {'a', 'b', 'i'}
+
+strexp = "sa - sa * ( sb + sc )"
+result = process_expr(strexp, values)
+print(f"Result: {result}")
+assert result == {'b'}
+
+strexp = "sc + sa * ( sb + sc * ( sb + sc ) - sa )"
+result = process_expr(strexp, values)
+print(f"Result: {result}")
+# assert result == {'b'}
+
+x = values['sb'] | values['sc']
+print(f"sb + sc = {x}")
+x = values['sc'] & x
+print(f"sc * (sb + sc) = {x}")
+x = values['sb'] | x
+print(f"(sb + sc * (sb + sc)) = {x}")
+x = x - values['sa']
+print(f"(sb + sc * (sb + sc) - sa) = {x}")
+x = values['sa'] & x
+print(f"sa * (sb + sc * (sb + sc) - sa) = {x}")
+x = values['sc'] | x
+print(f"sc + sa * (sb + sc * (sb + sc) - sa) = {x}")
+
